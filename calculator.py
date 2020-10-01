b6c11b51ab46570690facbd4fee6eab97e9f2782
sharklab7 <55528399+sharklab7@users.noreply.github.com>
2020-10-01 11:40:30 +0530

    Update calculator.py
    1. Setting the default resolution for the output window.
    2. The layout of the window is fixed now. 
    3. Few changes in layout and padding.
    4. Entry widget font size increased. (more visibility)

--- calculator.py
+++ calculator.py
@@ -1,87 +1,97 @@
-from Tkinter import Tk
-from Tkinter import StringVar,Entry,Button
-from math import pi,e,sin,cos,tan,log,log10,ceil,degrees,radians,exp,asin,acos,floor
+from tkinter import Tk
+from tkinter import StringVar, Entry, Button
+from math import pi, e, sin, cos, tan, log, log10, ceil, degrees, radians, exp, asin, acos, floor
 
 
 class calculator:
     def __init__(self):
-        window=Tk()
+        window = Tk()
         window.title('Scientific Calculator')
+
+        # default resolution
+        window.geometry("412x412")
+        window.maxsize(412, 412)       
+        window.minsize(412, 412)
+
         window.configure(background="white")
-        self.string=StringVar()
-        entry=Entry(window,textvariable=self.string)
-        entry.grid(row=0,column=0,columnspan=6)
+        self.string = StringVar()
+        entry = Entry(window, textvariable=self.string, font="arial 18")         
+        entry.grid(row=0, column=0, columnspan=6,pady=4)
         entry.configure(background="white")
         entry.focus()
-        
-        values=["7","8","9","/","%","clear","AC",
-                "4","5","6","*","(",")","**",
-                "1","2","3","-","=",",","0",".","min","+","sin","asin","cos","acos","tan()",
-                "pow","log10","max","abs","floor","pi","e","log","ceil","degrees","radians"]
-        text=1
-        i=0
-        row=1
-        col=0
+
+        values = ["7", "8", "9", "/", "%", "clear", "AC",
+                  "4", "5", "6", "*", "(", ")", "**",
+                  "1", "2", "3", "-", "=", ",", "0", ".", "min", "+", "sin", "asin", "cos", "acos", "tan()",
+                  "pow", "log10", "max", "abs", "floor", "pi", "e", "log", "ceil", "degrees", "radians"]
+        text = 1
+        i = 0
+        row = 1
+        col = 0
         for txt in values:
-            padx=10
-            pady=10
-            if(i==7):
-                row=2
-                col=0
-            if(i==14):
-                row=3
-                col=0
-            if(i==19):
-                row=4
-                col=0
-            if(i==26):
-                row=5
-                col=0
-            if(i==33):
-                row=6
-                col=0
-            if(txt=='='):
-                btn=Button(window,height=2,width=4,padx=70,pady=pady,text=txt,command=lambda txt=txt:self.equals())
-                btn.grid(row=row,column=col,columnspan=3,padx=2,pady=2)
+            padx = 10
+            pady = 10
+            if (i == 7):
+                row = 2
+                col = 0
+            if (i == 14):
+                row = 3
+                col = 0
+            if (i == 19):
+                row = 4
+                col = 0
+            if (i == 26):
+                row = 5
+                col = 0
+            if (i == 33):
+                row = 6
+                col = 0
+            if (txt == '='):
+                btn = Button(window, height=2, width=4, padx=70, pady=pady, text=txt,
+                             command=lambda txt=txt: self.equals())
+                btn.grid(row=row, column=col, columnspan=3, padx=2, pady=2)
                 btn.configure(background="yellow")
 
-            elif(txt=='clear'):
-                btn=Button(window,height=2,width=4,padx=padx,pady=pady, text=txt ,command=lambda txt=txt:self.delete())
-                btn.grid(row=row,column=col,padx=1,pady=1)
+            elif (txt == 'clear'):
+                btn = Button(window, height=2, width=4, padx=padx, pady=pady, text=txt,
+                             command=lambda txt=txt: self.delete())
+                btn.grid(row=row, column=col, padx=1, pady=1)
                 btn.configure(background="grey")
-            elif(txt=='AC'):
-                btn=Button(window,height=2,width=4,padx=padx,pady=pady,text=txt,command=lambda txt=txt:self.clearall())
-                btn.grid(row=row,column=col,padx=1,pady=1)
+            elif (txt == 'AC'):
+                btn = Button(window, height=2, width=4, padx=padx, pady=pady, text=txt,
+                             command=lambda txt=txt: self.clearall())
+                btn.grid(row=row, column=col, padx=1, pady=1)
                 btn.configure(background="red")
             else:
-                btn=Button(window,height=2,width=4,padx=padx,pady=pady,text=txt ,command=lambda txt=txt:self.addChar(txt))
-                btn.grid(row=row,column=col,padx=1,pady=1)
+                btn = Button(window, height=2, width=4, padx=padx, pady=pady, text=txt,
+                             command=lambda txt=txt: self.addChar(txt))
+                btn.grid(row=row, column=col, padx=1, pady=1)
                 btn.configure(background="cyan")
 
-            col=col+1
-            i=i+1
+            col = col + 1
+            i = i + 1
         window.mainloop()
-        
 
     def clearall(self):
         self.string.set("")
 
     def equals(self):
-        result=""
+        result = ""
 
         try:
-            result=eval(self.string.get())
+            result = eval(self.string.get())
             self.string.set(result)
         except:
-            result="INVALID INPUT"
+            result = "INVALID INPUT"
         self.string.set(result)
-        
-    def addChar(self,char):
-        self.string.set(self.string.get()+(str(char)))
-        
+
+    def addChar(self, char):
+        self.string.set(self.string.get() + (str(char)))
+
     def delete(self):
         self.string.set(self.string.get()[0:-1])
-           
+
+
 calculator()
 
   
